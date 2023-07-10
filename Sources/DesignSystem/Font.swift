fce0b517bacf8cdb82b4fa116d1c8f9649659747
Jorge Mendoza <jorgevmendoza@gmail.com>
2023-07-10 16:22:04 +0200

    Fixed font type

--- Sources/DesignSystem/Font.swift
+++ Sources/DesignSystem/Font.swift
@@ -7,10 +7,10 @@ public extension Font {
     /// Xcode's autocomplete allows for easy discovery of design system fonts.
     /// At any call site that requires a font, type `Font.DesignSystem.<esc>`
     struct DesignSystem {
-        public static let title = Font.custom("Inter-Bold", size: 88)
-        public static let numbersTitle = Font.custom("Inter-Bold", size: 66)
-        public static let thinGreen = Font.custom("Inter-Light", size: 39)
-        public static let thin = Font.custom("Inter-Light", size: 28)
-        public static let regular = Font.custom("Inter-Light", size: 17)
+        public static let title = Font.custom("OpenSans-Bold", size: 88)
+        public static let numbersTitle = Font.custom("OpenSans-Bold", size: 66)
+        public static let thinGreen = Font.custom("OpenSans-Light", size: 39)
+        public static let thin = Font.custom("OpenSans-Light", size: 28)
+        public static let regular = Font.custom("OpenSans-Light", size: 17)
     }
 }
