166a867f436ddf73b765a5541e52389b67bceaba
Jorge Mendoza <jorgevmendoza@gmail.com>
2023-07-23 11:33:03 +0200

    new fonts

--- Sources/DesignSystem/Font.swift
+++ Sources/DesignSystem/Font.swift
@@ -8,9 +8,12 @@ public extension Font {
     /// At any call site that requires a font, type `Font.DesignSystem.<esc>`
     struct DesignSystem {
         public static let title = Font.custom("OpenSans-Bold", size: 88)
+        public static let title2 = Font.custom("OpenSans-Bold", size: 48)
+        public static let title3 = Font.custom("OpenSans-Bold", size: 24)
         public static let numbersTitle = Font.custom("OpenSans-Bold", size: 66)
         public static let thinGreen = Font.custom("OpenSans-Light", size: 39)
         public static let thin = Font.custom("OpenSans-Light", size: 28)
-        public static let regular = Font.custom("OpenSans-Light", size: 17)
+        public static let regular = Font.custom("OpenSans-Light", size: 18)
+        public static let regular2 = Font.custom("OpenSans-Light", size: 14)
     }
 }
